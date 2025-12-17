cca1eca53a18f19710b77ac67d12ba7a1cbdb94a
Adarsh Sreekumar <adarshsreekumar298@gmail.com>
2025-12-17 23:52:12 +0530

    ADD : book images

--- routes/routing.js
+++ routes/routing.js
@@ -18,10 +18,22 @@ router.post('/login',userController.loginController)
 // google login
 router.post('/google/sign-in',userController.googleLoginController)
 
+// get home books
+router.get('/books/home',bookController.getHomePageBooksController)
+
 // ----------------------authorised user---------------------------
 
 // add book - request body content is form data (file type) so we use "multer"
 router.post('/user/book/add',jwtMiddleware,multerMiddleware.array('uploadImages',3),bookController.addBookController)
+
+// get all books page
+router.get('/books/all',jwtMiddleware,bookController.getUserAllBookPageController)
+
+// get all user upload books page
+router.get('/user-books/all',jwtMiddleware,bookController.getUserUploadProfilePageController)
+
+// get all user bought books page
+router.get('/user-boughtbooks/all',jwtMiddleware,bookController.getUserBoughtBookProfilePageController)
 
 
 
